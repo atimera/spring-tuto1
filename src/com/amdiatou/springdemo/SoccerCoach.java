f0adb309e1612168c7fc517ee7509f4a76496661
laenzo <amdiatoutimera@hotmail.fr>
2018-11-22 00:24:10 +0100

    Spring IoC maintenant plus besoin de toucher au code source, juste le fichier applicationContext.xml

---
+++ src/com/amdiatou/springdemo/SoccerCoach.java
@@ -0,0 +1,11 @@
+package com.amdiatou.springdemo;
+
+public class SoccerCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Faire 5 tours de terrain et faire des étirements.";
+	}
+
+}
