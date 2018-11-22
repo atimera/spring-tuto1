17843c976521b6f16ca63128d2eb7f2734030864
laenzo <amdiatoutimera@hotmail.fr>
2018-11-22 04:25:22 +0100

    Cycle de vie des beans: init-method et detroy-method

--- src/com/amdiatou/springdemo/TrackCoach.java
+++ src/com/amdiatou/springdemo/TrackCoach.java
@@ -23,5 +23,14 @@ public class TrackCoach implements Coach {
 		// use my fortuneService to get a fortune
 		return "Just Do It: "+ fortuneService.getFortune();
 	}
-
+	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add an destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");
+	}
 }
